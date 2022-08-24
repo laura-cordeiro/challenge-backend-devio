8f78eff6c803c677032f5c90fe8258874e686f63
Laura Cordeiro <92588463+laura-cordeiro@users.noreply.github.com>
2022-08-23 21:30:58 -0300

    feat(payments): add createPaymentByOrderId

--- src/domain/payments/paymentsController.ts
+++ src/domain/payments/paymentsController.ts
@@ -12,5 +12,18 @@ const PaymentsController = {
       res.json({ message: err.message });
     }
   },
+
+  async createPaymentByOrderId(req: Request, res: Response) {
+    try {
+      const { orderId } = req.params;
+      const { type } = req.body;
+      const data = await PaymentsService.createPaymentByOrderId(orderId, type);
+      res.status(200);
+      res.json(data);
+    } catch (err: any) {
+      res.status(400);
+      res.json({ message: err.message });
+    }
+  },
 };
 export default PaymentsController;
--- src/routes/paymentsRoutes.ts
+++ src/routes/paymentsRoutes.ts
@@ -4,5 +4,9 @@ import PaymentsController from '../domain/payments/paymentsController';
 const paymentsRouter = express.Router();
 
 paymentsRouter.get('/payments', PaymentsController.getPaymentsOrders);
+paymentsRouter.post(
+  '/payments/:orderId',
+  PaymentsController.createPaymentByOrderId,
+);
 
 export default paymentsRouter;
